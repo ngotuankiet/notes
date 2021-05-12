cf82b359d81c39b3ddc6bebe6ed38d445d9b1c7b
Kiet Ngo <ngotuankiet@github.com>
2021-05-12 18:05:39 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+pip install django django-rest-framework django-cors-headers
+pip install djangorestframework_simplejwt
+pip install -U drf-yasg
---
+++ project/jwtauth/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import registration
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+urlpatterns = [
+    path("register/", registration, name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("docs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
